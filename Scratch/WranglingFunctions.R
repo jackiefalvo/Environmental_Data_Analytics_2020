52d44e1ec29a8729b3ac8a5c70724a0afc3bf780
Cristiana Falvo <cristiana@Cristianas-MacBook-Pro.local>
2020-01-30 13:10:59 -0500

    added notes to data wrangling (lesson 7) and scratch functions notes

--- Scratch/WranglingFunctions.R
+++ Scratch/WranglingFunctions.R
@@ -1,8 +1,20 @@
-filter() # chooses specific data based on 
+filter() # chooses specific data based on criteria; temporary (like sel by attribute in GIS)
 arrange() # 
-select() # select based on name of column; can say include and not include something
+select() # grabs certain columns
+# select based on name of column; can say include and not include something
     # select(data, -lakename, sampledate) # exclude lakename, include sampledate
     # select(data, col1:col2, new col, col3 - col7) # inserts a new column in between col 2 and 3
 mutate() # adds something onto the end of the dataset
 lubridate() # 
 interval() # comes with lubridate
+
+## go over these functions and packages, become a bit more familiar
+  # filter and piping can particularly be useful I think
+  # also writing new csvs
+
+gather() # switches short to long format !! wow !! this is exactly what I need Kateri!
+spread() # switches long to short format
+separate() # can separate dates for instance Y/M/D -> seperate Y, M, D columns
+unite() # opposite of separate
+
+join() # 
