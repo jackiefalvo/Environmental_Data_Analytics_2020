c165d0467fcf562f0270da6a6e4b9037fae10f12
Cristiana Falvo <cristiana@Cristianas-MacBook-Pro.local>
2020-02-15 22:22:09 -0500

    completed assignment 5, added questiont o scratch sheet

--- Scratch/Questions.R
+++ Scratch/Questions.R
@@ -6,3 +6,7 @@ rbind() vs full_join()
 # what does the c() mean..
 c("thing 1", "thing 2", "etc.") # is this like a python list?
 
+# visualization
+  # adjusting legends
+  # in homework 5, making 3 sep box plots that we then combined into cowplot
+    # box plots are side by side but still measuring change over time?
